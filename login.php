7493bfea46007499d42b9951cf3a18eb324a3337
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-03-27 16:27:01 -0500

    creating the login page

--- login.php
+++ login.php
@@ -18,30 +18,21 @@
 
                 <div class="header">
                     <img src="assets/images/logo.png" title="Logo" alt="Site Logo"/>
-                    <h3> Sign Up </h3>
+                    <h3> Sign In </h3>
                     <span> to continue to Bachflix </span>
                 </div>
 
                 <form method="POST">
-                    <input type="text" name="firstName" placeholder="First Name" required>
-
-                    <input type="text" name="lastName" placeholder="Last Name" required>
 
                     <input type="text" name="username" placeholder="Username" required>
 
-                    <input type="email" name="email" placeholder="example@example.com" required>
-
-                    <input type="email" name="email2" placeholder="Confirm email" required>
-
-                    <input type="password" name="password" placeholder="Enter a safe password" required>
-
-                    <input type="password" name="password2" placeholder="Confirm password" required>
+                    <input type="password" name="password" placeholder="Password" required>
 
                     <input type="submit" name="submitButton" value="SUBMIT">
 
                 </form>
 
-                <a href="login.php" class="signInMessage"> Already have an account? Sign in here! </a>
+                <a href="register.php" class="signInMessage"> Need an account? Sign up here! </a>
 
             </div>
 
