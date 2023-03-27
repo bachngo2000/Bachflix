cf1844e8b04e820a4a29d9edec5371eeb27227eb
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-03-27 15:42:26 -0500

    handling form submission

--- register.php
+++ register.php
@@ -1,3 +1,11 @@
+<?php
+
+    if(isset($_POST["submitButton"])) {
+        echo "Thank You! Your form was successfully submitted";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,6 +15,26 @@
     <body>
         <div class="signInContainer">
             <div class="column">
+                <form method="POST">
+                    <input type="text" name="firstName" placeholder="First Name" required>
+
+                    <input type="text" name="lastName" placeholder="Last Name" required>
+
+                    <input type="text" name="username" placeholder="Username" required>
+
+                    <input type="email" name="email" placeholder="example@example.com" required>
+
+                    <input type="email" name="email2" placeholder="Confirm email" required>
+
+                    <input type="password" name="password" placeholder="Enter a safe password" required>
+
+                    <input type="password" name="password2" placeholder="Confirm password" required>
+
+                    <input type="submit" name="submitButton" value="SUBMIT">
+
+
+
+                </form>
             </div>
 
         </div>
