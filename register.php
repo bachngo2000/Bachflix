733041e5afc8ca6c88a377dfbd7cd8ef8537d131
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-03-27 15:00:42 -0500

    setting up register page

---
+++ register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Welcome to Bachflix </title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    <body>
+        hotdogs motherfuker
+        
+
+    </body>
+    
+</html>
