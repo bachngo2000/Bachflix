63cd5c6d44b2963a906c5a7adbbeb0eec1ef3a0e
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-03-27 20:51:14 -0500

    add sanitation methods

--- includes/classes/FormSanitizer.php
+++ includes/classes/FormSanitizer.php
@@ -1,6 +1,6 @@
 <?php
     class FormSanitizer {
-        
+
         public static function sanitizeFormString($inputText) {
             $inputText = strip_tags($inputText);
             $inputText = str_replace(" ", "", $inputText);
@@ -9,5 +9,24 @@
             $inputText = ucfirst($inputText);
             return $inputText;
         }
+
+        public static function sanitizeFormUsername($inputText) {
+            $inputText = strip_tags($inputText);
+            $inputText = str_replace(" ", "", $inputText);
+            return $inputText;
+        }
+
+        public static function sanitizeFormPassword($inputText) {
+            $inputText = strip_tags($inputText);
+            return $inputText;
+        }
+
+        public static function sanitizeFormEmail($inputText) {
+            $inputText = strip_tags($inputText);
+            $inputText = str_replace(" ", "", $inputText);
+            return $inputText;
+        }
+
+
     }
 ?>
