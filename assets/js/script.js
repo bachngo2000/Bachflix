dfa2bb43b95c19a945236a7efbf3f940b670e47b
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-26 16:43:15 -0500

    changed the icon on mute/unmute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 function volumeToggle(button) {
     var muted = $(".previewVideo").prop("muted");
     $(".previewVideo").prop("muted", !muted);
+
+    $(button).find("i").toggleClass("fa-volume-xmark");
+    $(button).find("i").toggleClass("fa-volume-high");
+
 }
