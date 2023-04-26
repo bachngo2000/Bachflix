96b664551f37e12b3b1eb24ac9958303954572a9
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-26 10:54:21 -0500

    added getter functions for the Entities class

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -1,29 +1,38 @@
 <?php
-    class PreviewProvider {
+class PreviewProvider {
 
-        private $con, $username;
+    private $con, $username;
 
-        public function __construct($con, $username) {
-            $this->con = $con;
-            $this->username = $username;
-
-        }
-
-        public function createPreviewVideo($entity) {
-            if ($entity == null) {
-                $entity = $this->getRandomEntity();
-            }
-        }
-
-        private function getRandomEntity() {
-
-            $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
-            $query->execute();
-
-            $row = $query->fetch(PDO::FETCH_ASSOC);
-
-            echo $row["name"];
-
-        }
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
     }
+
+    public function createPreviewVideo($entity) {
+        
+        if($entity == null) {
+            $entity = $this->getRandomEntity();
+        }
+
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $preview = $entity->getPreview();
+        $thumbnail = $entity->getThumbnail();
+
+        // test
+        echo "<img src='$thumbnail'>";
+
+    }
+
+    private function getRandomEntity() {
+
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query->execute();
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        
+        return new Entity($this->con, $row);
+    }
+
+}
 ?>
