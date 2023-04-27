a652e3683f1ba62988cbc38100f1099e2dac50c1
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-27 14:04:59 -0500

    up next title

--- watch.php
+++ watch.php
@@ -7,19 +7,31 @@ if(!isset($_GET["id"])) {
 
 $video = new Video($con, $_GET["id"]);
 $video->incrementViews();
+
+$upNextVideo = VideoProvider::getUpNext($con, $video);
+
 ?>
 <div class="watchContainer">
 
-<div class="videoControls watchNav">
-    <button onclick="goBack()"><i class="fa-solid fa-arrow-left"></i></button>
-    <h1><?php echo $video->getTitle(); ?></h1>
-</div>
+    <div class="videoControls watchNav">
+        <button onclick="goBack()"><i class="fa-solid fa-arrow-left"></i></button>
+        <h1><?php echo $video->getTitle(); ?></h1>
+    </div>
 
+    <div class="videoControls upNext">
 
+        <button><i class="fa-solid fa-rotate-right"></i></button>
 
-<video controls autoplay>
-    <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
-</video>
+        <div class="upNextContainer">
+            <h2>Up next:</h2>
+            <h3><?php echo $upNextVideo->getTitle(); ?></h3>
+        </div>
+
+    </div>
+
+    <video controls autoplay>
+        <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
+    </video>
 </div>
 
 <script>
