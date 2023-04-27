0cc2238a7b9e76151189bd6e5a65b250ee3424f3
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-27 09:45:58 -0500

    added go back button

--- watch.php
+++ watch.php
@@ -8,3 +8,16 @@ if(!isset($_GET["id"])) {
 $video = new Video($con, $_GET["id"]);
 $video->incrementViews();
 ?>
+<div class="watchContainer">
+
+<div class="videoControls watchNav">
+    <button><i class="fa-solid fa-arrow-left"></i></button>
+    <h1><?php echo $video->getTitle(); ?></h1>
+</div>
+
+
+
+<video controls autoplay>
+    <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
+</video>
+</div>
