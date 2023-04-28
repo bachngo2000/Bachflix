3ba2dd54d736d540c598715d35be0720dd82fca1
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-27 21:08:23 -0500

    styled tv show categories

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -20,6 +20,20 @@ class CategoryContainers {
 
         return $html . "</div>";
     }
+
+    public function showTVShowCategories() {
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+
+        $html = "<div class='previewCategories'>
+                    <h1>TV Shows</h1>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $html .= $this->getCategoryHtml($row, null, true, false);
+        }
+
+        return $html . "</div>";
+    }
     
     public function showCategory($categoryId, $title = null) {
         $query = $this->con->prepare("SELECT * FROM categories WHERE id=:id");
@@ -43,10 +57,10 @@ class CategoryContainers {
             $entities = EntityProvider::getEntities($this->con, $categoryId, 30);
         }
         else if($tvShows) {
-            // Get tv show entities
+            $entities = EntityProvider::getTVShowEntities($this->con, $categoryId, 30);
         }
         else {
-            // Get movie entities
+            // $entities = EntityProvider::getMoviesEntities($this->con, $categoryId, 30);
         }
 
         if(sizeof($entities) == 0) {
