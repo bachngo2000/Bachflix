0f45c4729a860b3e27fff0b341c12acb77d0eca7
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2023-04-27 21:27:02 -0500

    movie page preview video and movie categories

--- movies.php
+++ movies.php
@@ -2,8 +2,8 @@
 require_once("includes/header.php");
 
 $preview = new PreviewProvider($con, $userLoggedIn);
-echo $preview->createTVShowPreviewVideo();
+echo $preview->createMoviesPreviewVideo();
 
 $containers = new CategoryContainers($con, $userLoggedIn);
-echo $containers->showTVShowCategories();
+echo $containers->showMovieCategories();
 ?>
